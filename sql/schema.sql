99c298f576d3933f32ba0f47f9fa7d92fa637aa4
seanianking <seanianking@gmail.com>
2020-04-05 17:46:56 -0600

    updated schema with seed information

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,29 +5,39 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 -- Creates the table "favorite_foods" within favorite_db --
-CREATE TABLE favorite_foods (
-  -- Makes a string column called "food" which cannot contain null --
-  food VARCHAR(50) NOT NULL,
-  -- Makes an numeric column called "score" --
-  score INTEGER(10)
-);
 
-CREATE TABLE departments ( 
+CREATE TABLE department ( 
   id  INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(30) NOT NULL,
-  
+  name VARCHAR(30) NOT NULL,
+
   PRIMARY KEY (id)
 );
 
-CREATE TABLE roles ( 
+INSERT INTO department (name)
+VALUE 
+("web_development"), 
+("product"), 
+("customer_service");
+
+USE tracker_db;
+
+CREATE TABLE role ( 
   id  INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(6, 2) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
   department_id INT NOT NULL,
 
   PRIMARY KEY (id)
 
 );
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Web Engineer", 70000.00, 1),
+("Software Developer", 80000.00, 2),
+("Phone Agent", 40000.00, 3);
+
+
+USE tracker_db;
 
 CREATE TABLE employee ( 
   id  INT NOT NULL AUTO_INCREMENT,
@@ -38,3 +48,9 @@ CREATE TABLE employee (
 
   PRIMARY KEY (id)
 );
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sean", "King", 1, 1),
+    ("Nick", "Thomas", 2, 2),
+    ("Tucker", "Vassau", 3, 3),
+    ("Tyler", "Snyder", 4, 4);
