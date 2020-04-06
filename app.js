f6c269869c5f86b4cff6806de0e4e7fe64db1438
seanianking <seanianking@gmail.com>
2020-04-06 05:17:35 -0600

    added functions to add and view information, added spaces to input related inquirer functions

--- app.js
+++ app.js
@@ -17,3 +17,40 @@ connection.connect(function(err){
     init();
 });
 
+// functions are needed to add employees to the 
+// database, view employees currently in the 
+// database, and change information that is already
+// present in the database. inquirer function
+// is used to determine which of those functions is
+// called.
+
+//function to add information
+function insertInfo(table, response){
+    let queryString = 'INSERT INTO ?? SET ?';
+    connection.query(queryString, [table, response], function(err, result){
+        if (err) throw err;
+        console.log('info added')
+    })
+    init();
+};
+
+//inquirer function to determine which table to add to
+
+
+//pull information to view
+function viewInfo(table){
+    let queryString = 'SELECT * FROM ??';
+    connection.query(queryString, [table], function(err, result){
+        if (err) throw err;
+        console.log('Viewing ' + table)
+        console.log(result);
+    });
+    init();
+};
+
+//inquirer function to determine which table to view
+
+
+
+
+
